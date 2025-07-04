6f475c53a9acbe04338a5b20b3b603326abcbb3c
mhatrep802 <mhatrep802@gmail.com>
2025-07-04 09:11:06 -0400

    can deploy now

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/tracetutor-demo1/',
   plugins: [
     react(),
     tailwindcss(),
